f2b112be3718fa3820624fddf85a31927c933496
Sansone11 <sansonemirco@gmail.com>
2022-09-12 14:51:30 +0200

    card-edit

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="nav">
-    <input class="search" type="text">
+    <input class="search" type="text" v-model="query">
     <button @click="searchMovies">Search Movie</button>
     <div>
       <ul>
-        <li v-for="film in movies" :key="film.id" >
-        <h3>{{film.original_title}}</h3>
-        <p>{{film.title}}</p>
-        <p>{{film.original_language}}</p>
-        <p>{{film.vote_average}}</p>
-      </li>  
+        <li v-for="film in movies" :key="film.id">
+          <h3>{{film.original_title}}</h3>
+          <p>{{film.title}}</p>
+          <p>{{film.original_language}}</p>
+          <p>{{film.vote_average}}</p>
+        </li>
       </ul>
     </div>
 
@@ -20,30 +20,34 @@
 import axios from 'axios';
 import state from '../components/store';
 export default {
+  data() {
+    return {
+      query: "",
+    }
+  },
   props: {
     film: Array,
-    
+
   },
   methods: {
     searchMovies() {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=87f0771b157e9b64c497826e9e19fe61&query=pippo')
+      axios.get(`${state.baseUri}/search/movie?api_key=${state.apikey}&query=${this.query}&language=it-IT`)
         .then((res) => {
-          console.log(res.data)
           state.movies = res.data.results
-
+          console.log('movies:',state.movie)
         })
     },
   },
-  created() {
-    this.searchMovies()
-  },
+ 
   computed: {
-    movies(){
+    movies() {
       return state.movies
-        
-      
-    }
+
+
+    },
+
   },
+
 }
 console.log(state.movies)
 </script>
